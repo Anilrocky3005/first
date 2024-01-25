c79acd2ba7c1cd4619d64418cb8bde17e70f1601
rocky <ganjianil@outlook.com>
2024-01-25 22:21:28 +0530

    mess

--- src/Check.jsx
+++ src/Check.jsx
@@ -6,7 +6,9 @@ function Check() {
         <div className='container'>
             <div className='footer mt-5'>
                 <p> Designed by  ganjianil@outlook.com</p>
+                
             </div>
+            <div>hi all thes is a normal demo check and checking its working or not</div>
         </div>
     </div>
   )
